6d8b52432cf7b086cc0da309c9e05943ed388639
sgraham9319 <sgraham3@uw.edu>
2021-09-24 12:01:10 -0700

    Created multi-panel figure

--- PSME_growth_model.R
+++ PSME_growth_model.R
@@ -1,6 +1,7 @@
 library(ForestPlot)
 library(dplyr)
 library(ggplot2)
+library(ggpubr)
 
 #====================================
 # Part 1. Selecting neighborhood size
@@ -87,14 +88,17 @@ for(i in 1:length(radii)){
 nb_comp_ABAM <- ggplot(data = nb_rad_comp, aes(x = radius, y = ABAM_mse)) +
   geom_line(col = "green") +
   labs(x = "Neighborhood radius (m)", y = "Mean square error") +
+  ggtitle("ABAM") +
   theme_classic()
 nb_comp_PSME <- ggplot(data = nb_rad_comp, aes(x = radius, y = PSME_mse)) +
   geom_line(col = "green") +
   labs(x = "Neighborhood radius (m)", y = "Mean square error") +
+  ggtitle("PSME") +
   theme_classic()
 nb_comp_TSHE <- ggplot(data = nb_rad_comp, aes(x = radius, y = TSHE_mse)) +
   geom_line(col = "green") +
   labs(x = "Neighborhood radius (m)", y = "Mean square error") +
+  ggtitle("TSHE") +
   theme_classic()
 
 # Chosen neighborhood sizes: ABAM = 12m, PSME = 10m, TSHE = 12m
@@ -103,22 +107,16 @@ nb_comp_TSHE <- ggplot(data = nb_rad_comp, aes(x = radius, y = TSHE_mse)) +
 # Part 2. Fitting final model
 #============================
 
-# Select focal species for fitting final model
-sps <- "ABAM"
-
-# Define optimal neighborhood size
-if(sps == "PSME"){
-  nb_rad <- 10
-} else {
-  nb_rad <- 12
-}
+#----------------
+# 2.1. ABAM model
+#----------------
 
 # Construct neighborhoods
-nbhds <- neighborhoods(mapping, radius = nb_rad)
+nbhds <- neighborhoods(mapping, radius = 12)
 
 # Describe neighborhoods
-nbhd_summ <- neighborhood_summary(nbhds, id_column = "tree_id", radius = nb_rad,
-                                  densities = "proportional")
+nbhd_summ <- neighborhood_summary(nbhds, id_column = "tree_id",
+                                  radius = 12, densities = "proportional")
 
 # Combine neighborhoods with their summaries
 nbhds <- nbhds %>%
@@ -126,117 +124,209 @@ nbhds <- nbhds %>%
 
 # Remove trees whose max neighborhood overlaps a plot boundary
 nbhds <- nbhds %>%
-  filter(x_coord >= nb_rad & x_coord <= 100 - nb_rad &
-           y_coord >= nb_rad & y_coord <= 100 - nb_rad)
-
-# Calculate annual growth for all trees
-growth <- growth_summary(tree)
-
-# Remove trees that were only measured once and/or had negative growth
-growth <- growth %>%
-  filter(first_record != last_record &
-           annual_growth >= 0)
+  filter(x_coord >= 12 & x_coord <= 100 - 12 &
+           y_coord >= 12 & y_coord <= 100 - 12)
 
 # Add growth data to neighborhoods
 nbhds <- nbhds %>%
-  inner_join(growth %>% select(tree_id, size_corr_growth),
-             by = "tree_id")
+  inner_join(growth %>% select(tree_id, size_corr_growth), by = "tree_id")
 
 # Add plot abiotic data
 nbhds <- nbhds %>%
   left_join(stand_abiotic, by = "stand_id")
 
-# Subset to focal species
+# Subset to focal species and drop unneeded columns
 nbhds <- nbhds %>%
-  filter(species == sps)
-
-# Drop columns not needed for the model
-nbhds <- nbhds %>%
+  filter(species == "ABAM") %>%
   select(-c(stand_id, species, dbh, abh, x_coord, y_coord, id_comp, abh_comp))
 
-# Run model
-final_mod <- growth_model(nbhds, outcome_var = "size_corr_growth",
-                          iterations = 50, rare_comps = 100,
-                          density_suffix = "_density")
+# Fit model
+ABAM_mod <- growth_model(nbhds, outcome_var = "size_corr_growth",
+                         iterations = 10, rare_comps = 100,
+                         density_suffix = "_density")
 
 # Format data for plotting
-if(sps == "ABAM"){
-  coef_summ <- final_mod$mod_coef %>%
-    mutate(ABAM = 0.5 * (sps_compABAM + ABAM_density),
-           ABLA = 0.5 * (sps_compABLA + ABLA_density),
-           CANO = 0.5 * (sps_compCANO + CANO_density),
-           PSME = 0.5 * (sps_compPSME + PSME_density),
-           RARE = 0.5 * (sps_compRARE + RARE_density),
-           TABR = 0.5 * (sps_compTABR + TABR_density),
-           THPL = 0.5 * (sps_compTHPL + THPL_density),
-           TSHE = 0.5 * (sps_compTSHE + TSHE_density),
-           TSME = 0.5 * (sps_compTSME + TSME_density)) %>%
-    select(ABAM, ABLA, CANO, PSME, RARE, TABR, THPL, TSHE, TSME)
-  plot_data <- data.frame(
-    competitor <- c("ABAM", "ABLA", "CANO", "PSME", "RARE", "TABR", "THPL",
-                    "TSHE", "TSME"),
-    means <- apply(coef_summ, MARGIN = 2, FUN = mean),
-    sds <- apply(coef_summ, MARGIN = 2, FUN = sd)
-  )
-} else if(sps == "PSME"){
-  coef_summ <- final_mod$mod_coef %>%
-    mutate(ABAM = 0.5 * (sps_compABAM + ABAM_density),
-           ABLA = 0.5 * (sps_compABLA + ABLA_density),
-           PICO = 0.5 * (sps_compPICO + PICO_density),
-           PIMO = 0.5 * (sps_compPIMO + PIMO_density),
-           PSME = 0.5 * (sps_compPSME + PSME_density),
-           RARE = 0.5 * (sps_compRARE + RARE_density),
-           THPL = 0.5 * (sps_compTHPL + THPL_density),
-           TSHE = 0.5 * (sps_compTSHE + TSHE_density)) %>%
-    select(ABAM, ABLA, PICO, PIMO, PSME, RARE, THPL, TSHE)
-  plot_data <- data.frame(
-    competitor <- c("ABAM", "ABLA", "PICO", "PIMO", "PSME", "RARE", "THPL",
-                    "TSHE"),
-    means <- apply(coef_summ, MARGIN = 2, FUN = mean),
-    sds <- apply(coef_summ, MARGIN = 2, FUN = sd)
-  )
-} else if(sps == "TSHE"){
-  coef_summ <- final_mod$mod_coef %>%
-    mutate(ABAM = 0.5 * (sps_compABAM + ABAM_density),
-           ABLA = 0.5 * (sps_compABLA + ABLA_density),
-           CANO = 0.5 * (sps_compCANO + CANO_density),
-           PICO = 0.5 * (sps_compPICO + PICO_density),
-           PIMO = 0.5 * (sps_compPIMO + PIMO_density),
-           PSME = 0.5 * (sps_compPSME + PSME_density),
-           RARE = 0.5 * (sps_compRARE + RARE_density),
-           TABR = 0.5 * (sps_compTABR + TABR_density),
-           THPL = 0.5 * (sps_compTHPL + THPL_density),
-           TSHE = 0.5 * (sps_compTSHE + TSHE_density),
-           TSME = 0.5 * (sps_compTSME + TSME_density)) %>%
-    select(ABAM, ABLA, CANO, PICO, PIMO, PSME, RARE, TABR, THPL, TSHE, TSME)
-  plot_data <- data.frame(
-    competitor <- c("ABAM", "ABLA", "CANO", "PICO", "PIMO", "PSME", "RARE",
-                    "TABR", "THPL", "TSHE", "TSME"),
-    means <- apply(coef_summ, MARGIN = 2, FUN = mean),
-    sds <- apply(coef_summ, MARGIN = 2, FUN = sd)
-  )
-}
+coef_summ <- ABAM_mod$mod_coef %>%
+  mutate(ABAM = 0.5 * (sps_compABAM + ABAM_density),
+         ABLA = 0.5 * (sps_compABLA + ABLA_density),
+         CANO = 0.5 * (sps_compCANO + CANO_density),
+         PSME = 0.5 * (sps_compPSME + PSME_density),
+         RARE = 0.5 * (sps_compRARE + RARE_density),
+         TABR = 0.5 * (sps_compTABR + TABR_density),
+         THPL = 0.5 * (sps_compTHPL + THPL_density),
+         TSHE = 0.5 * (sps_compTSHE + TSHE_density),
+         TSME = 0.5 * (sps_compTSME + TSME_density)) %>%
+  select(ABAM, ABLA, CANO, PSME, RARE, TABR, THPL, TSHE, TSME)
+plot_data_ABAM <- data.frame(
+  competitor = c("ABAM", "ABLA", "CANO", "PSME", "RARE", "TABR", "THPL",
+                  "TSHE", "TSME"),
+  means = apply(coef_summ, MARGIN = 2, FUN = mean),
+  sds = apply(coef_summ, MARGIN = 2, FUN = sd)
+)
 
 # Create plot
-ggplot(plot_data, aes(x = competitor, y = means)) +
+sps_int_ABAM <- ggplot(plot_data_ABAM, aes(x = competitor, y = means)) +
   geom_point() +
   geom_errorbar(aes(ymin = means - sds, ymax = means + sds), width = 0) +
   geom_hline(yintercept = 0, linetype = "dashed") +
-  ggtitle(sps) +
-  theme_classic()
+  xlab("Neighbor species") +
+  ylab("Growth response") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#----------------
+# 2.2. PSME model
+#----------------
+
+# Construct neighborhoods
+nbhds <- neighborhoods(mapping, radius = 10)
+
+# Describe neighborhoods
+nbhd_summ <- neighborhood_summary(nbhds, id_column = "tree_id",
+                                  radius = 10, densities = "proportional")
+
+# Combine neighborhoods with their summaries
+nbhds <- nbhds %>%
+  left_join(nbhd_summ, by = "tree_id")
+
+# Remove trees whose max neighborhood overlaps a plot boundary
+nbhds <- nbhds %>%
+  filter(x_coord >= 10 & x_coord <= 100 - 10 &
+           y_coord >= 10 & y_coord <= 100 - 10)
+
+# Add growth data to neighborhoods
+nbhds <- nbhds %>%
+  inner_join(growth %>% select(tree_id, size_corr_growth), by = "tree_id")
+
+# Add plot abiotic data
+nbhds <- nbhds %>%
+  left_join(stand_abiotic, by = "stand_id")
+
+# Subset to focal species and drop unneeded columns
+nbhds <- nbhds %>%
+  filter(species == "PSME") %>%
+  select(-c(stand_id, species, dbh, abh, x_coord, y_coord, id_comp, abh_comp))
+
+# Fit model
+PSME_mod <- growth_model(nbhds, outcome_var = "size_corr_growth",
+                         iterations = 10, rare_comps = 100,
+                         density_suffix = "_density")
+
+# Format data for plotting
+coef_summ <- PSME_mod$mod_coef %>%
+  mutate(ABAM = 0.5 * (sps_compABAM + ABAM_density),
+         ABLA = 0.5 * (sps_compABLA + ABLA_density),
+         PICO = 0.5 * (sps_compPICO + PICO_density),
+         PIMO = 0.5 * (sps_compPIMO + PIMO_density),
+         PSME = 0.5 * (sps_compPSME + PSME_density),
+         RARE = 0.5 * (sps_compRARE + RARE_density),
+         THPL = 0.5 * (sps_compTHPL + THPL_density),
+         TSHE = 0.5 * (sps_compTSHE + TSHE_density)) %>%
+  select(ABAM, ABLA, PICO, PIMO, PSME, RARE, THPL, TSHE)
+plot_data_PSME <- data.frame(
+  competitor = c("ABAM", "ABLA", "PICO", "PIMO", "PSME", "RARE", "THPL",
+                  "TSHE"),
+  means = apply(coef_summ, MARGIN = 2, FUN = mean),
+  sds = apply(coef_summ, MARGIN = 2, FUN = sd)
+)
+
+# Create plot
+sps_int_PSME <- ggplot(plot_data_PSME, aes(x = competitor, y = means)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = means - sds, ymax = means + sds), width = 0) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  xlab("Neighbor species") +
+  ylab("Growth response") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#----------------
+# 2.3. TSHE model
+#----------------
+
+# Construct neighborhoods
+nbhds <- neighborhoods(mapping, radius = 12)
+
+# Describe neighborhoods
+nbhd_summ <- neighborhood_summary(nbhds, id_column = "tree_id",
+                                  radius = 12, densities = "proportional")
+
+# Combine neighborhoods with their summaries
+nbhds <- nbhds %>%
+  left_join(nbhd_summ, by = "tree_id")
+
+# Remove trees whose max neighborhood overlaps a plot boundary
+nbhds <- nbhds %>%
+  filter(x_coord >= 12 & x_coord <= 100 - 12 &
+           y_coord >= 12 & y_coord <= 100 - 12)
+
+# Add growth data to neighborhoods
+nbhds <- nbhds %>%
+  inner_join(growth %>% select(tree_id, size_corr_growth), by = "tree_id")
+
+# Add plot abiotic data
+nbhds <- nbhds %>%
+  left_join(stand_abiotic, by = "stand_id")
+
+# Subset to focal species and drop unneeded columns
+nbhds <- nbhds %>%
+  filter(species == "TSHE") %>%
+  select(-c(stand_id, species, dbh, abh, x_coord, y_coord, id_comp, abh_comp))
+
+# Fit model
+TSHE_mod <- growth_model(nbhds, outcome_var = "size_corr_growth",
+                         iterations = 10, rare_comps = 100,
+                         density_suffix = "_density")
+
+# Format data for plotting
+coef_summ <- TSHE_mod$mod_coef %>%
+  mutate(ABAM = 0.5 * (sps_compABAM + ABAM_density),
+         ABLA = 0.5 * (sps_compABLA + ABLA_density),
+         CANO = 0.5 * (sps_compCANO + CANO_density),
+         PICO = 0.5 * (sps_compPICO + PICO_density),
+         PIMO = 0.5 * (sps_compPIMO + PIMO_density),
+         PSME = 0.5 * (sps_compPSME + PSME_density),
+         RARE = 0.5 * (sps_compRARE + RARE_density),
+         TABR = 0.5 * (sps_compTABR + TABR_density),
+         THPL = 0.5 * (sps_compTHPL + THPL_density),
+         TSHE = 0.5 * (sps_compTSHE + TSHE_density),
+         TSME = 0.5 * (sps_compTSME + TSME_density)) %>%
+  select(ABAM, ABLA, CANO, PICO, PIMO, PSME, RARE, TABR, THPL, TSHE, TSME)
+plot_data_TSHE <- data.frame(
+  competitor = c("ABAM", "ABLA", "CANO", "PICO", "PIMO", "PSME", "RARE",
+                  "TABR", "THPL", "TSHE", "TSME"),
+  means = apply(coef_summ, MARGIN = 2, FUN = mean),
+  sds = apply(coef_summ, MARGIN = 2, FUN = sd)
+)
+
+# Create plot
+sps_int_TSHE <- ggplot(plot_data_TSHE, aes(x = competitor, y = means)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = means - sds, ymax = means + sds), width = 0) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  xlab("Neighbor species") +
+  ylab("Growth response") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 #==================================
 # Part 3. Creating multi-panel plot
 #==================================
-#library(gridExtra)
-library(gtable)
-library(grid)
-g1 <- ggplotGrob(nb_comp_ABAM)
-g2 <- ggplotGrob(nb_comp_PSME)
-g3 <- ggplotGrob(nb_comp_TSHE)
-g <- rbind(g1, g2, g3, size = "first")
-g$widths <- unit.pmax(g1$widths, g2$widths, g3$widths)
-grid.newpage()
-grid.draw(g)
 
-# Might want to look into ggarrange()
+# Initiate plot saving
+jpeg(filename = "Figure2.jpg",
+     type = "cairo",
+     units = "px",
+     width = 6000,
+     height = 4000,
+     pointsize = 12,
+     res = 500)
+
+# Create plot
+ggarrange(nb_comp_ABAM, nb_comp_PSME, nb_comp_TSHE,
+          sps_int_ABAM, sps_int_PSME, sps_int_TSHE,
+          labels = c("A", "B", "C", "D", "E", "F"),
+          ncol = 3, nrow = 2)
+
+# Save plot
+dev.off()
